18cd92529ad2544c0c81c6f8e258d04f3ef8cf49
unknown <pranapramudita@gmail.com>
2019-04-04 17:21:02 +0700

    page_menu cicil

--- application/controllers/song.php
+++ application/controllers/song.php
@@ -1,6 +1,6 @@
 <?php
 
-class song extends CI_Controller(){
+class song extends CI_Controller{
     
     public function __construct()
     {
---
+++ application/views/page_header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Genius | Song Lyrics & Knowledge</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src=".js"></script>
+</head>
+<body>
+    header
+</body>
+</html>
