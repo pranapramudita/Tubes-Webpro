8758db2551c4a7bbb39fc6f772432cbe25fd44f9
pranapramudita <pranapramudita@gmail.com>
2019-04-08 23:28:04 +0700

    front end belum kelar

--- application/views/page_footer.php
+++ application/views/page_footer.php
@@ -1,1 +1,51 @@
-footer
+
+
+        <!-- footer -->
+        <nav class="navbar navbar-footer">
+                <div class="container text-center">
+                <ul class="nav navbar-nav navbar-center">
+                    <li><a href="//www.azlyrics.com/adv.html">Advertise Here</a></li>
+                    <li><a href="//www.azlyrics.com/privacy.html">Privacy Policy</a></li>
+                    <li><a href="//www.azlyrics.com/copyright.html">DMCA Policy</a></li>
+                </ul>
+                </div> 
+            </nav>
+            <div class="footer-wrap">
+                <div class="container">
+                <small>
+                    All lyrics are property and copyright of their owners. All lyrics provided for educational purposes and personal use only.<br>
+                    <script type="text/javascript">
+                        curdate=new Date();
+                        document.write("<strong>Copyright &copy; 2000-"+curdate.getFullYear()+" AZLyrics.com<\/strong>");
+                    </script>
+                </small>
+                </div>
+            </div>
+
+            <script>
+            cf_page_artist = "";
+            cf_page_song = "";
+            cf_page_genre = "";
+            </script>
+            <script src="//cdn.clickfuse.com/publishers/azlyrics/single.min.js"></script>
+
+            <script type="text/javascript">
+
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-4309237-1']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+            </script>
+            <div id="CssFailCheck" class="hidden" style="height:1px;"></div>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+            <script>window.jQuery || document.write('<script src="//www.azlyrics.com/local/jquery.min.js"><\/script>')</script>
+            <script>
+            $(function () {
+            if ($('#CssFailCheck').is(':visible') === true) {
+                $('<link rel="stylesheet" type="text/css" href="//www.azlyrics.com/bs/css/bootstrap.min.css"><link rel="stylesheet" href="//www.azlyrics.com/bsaz.css">').appendTo('head');
+            }
+            });
--- application/views/page_header.php
+++ application/views/page_header.php
@@ -1,25 +1,84 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- title, icon, etc.-->
-    <title>Genius | Song Lyrics & Knowledge</title>
-    <link rel="shortcut icon" href="assets/icon.png">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- title, icon, etc.-->
+        <title>AZLyrics - Song Lyrics from A to Z</title>
+        <link rel="shortcut icon" href="assets/az_logo_tr.png">
 
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="application\views\header.css">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    
-    <!-- code -->
-       
-</body>
+    </head>
+
+    <body class="margin50">
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        
+        <!-- code -->
+        <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#search-collapse"><span class="glyphicon glyphicon-search"></span></button>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#artists-collapse"><span class="glyphicon glyphicon-th-list"></span></button>
+            <a class="navbar-brand" href="//www.azlyrics.com"><img alt="AZLyrics.com" class="pull-left" style="max-height:40px; margin-top:-10px;" src="//www.azlyrics.com/az_logo_tr.png"></a>
+            </div>
+            <ul class="collapse navbar-collapse nav navbar-nav" id="artists-collapse">
+            <li>
+            <div class="btn-group text-center" role="group">
+            <a class="btn btn-menu" href="//www.azlyrics.com/a.html">A</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/b.html">B</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/c.html">C</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/d.html">D</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/e.html">E</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/f.html">F</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/g.html">G</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/h.html">H</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/i.html">I</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/j.html">J</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/k.html">K</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/l.html">L</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/m.html">M</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/n.html">N</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/o.html">O</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/p.html">P</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/q.html">Q</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/r.html">R</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/s.html">S</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/t.html">T</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/u.html">U</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/v.html">V</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/w.html">W</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/x.html">X</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/y.html">Y</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/z.html">Z</a>
+            <a class="btn btn-menu" href="//www.azlyrics.com/19.html">#</a>
+            </div>
+            </li>
+            </ul>
+
+            <div class="collapse navbar-collapse" id="search-collapse">
+
+                <form class="navbar-form navbar-right search" method="get" action="//search.azlyrics.com/search.php" role="search">
+                <div class="input-group">  
+                <input type="text" class="form-control" placeholder="" name="q" id="q">
+                    <span class="input-group-btn">
+                        <button class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-search"></span> Search</button>
+                    </span>
+            </div>   
+            </form>
+
+            </div><!-- /.navbar-collapse -->
+            </div><!-- /.container -->
+        </nav>
+        
+    </body>
 </html>
