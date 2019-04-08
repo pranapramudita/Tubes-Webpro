33ff76905e446e47c31cae3312ea9b8a455bd481
vesheago <vesheafg@gmail.com>
2019-04-08 12:04:09 +0700

    coba push 2

--- application/controllers/song.php
+++ application/controllers/song.php
@@ -4,7 +4,7 @@ class song extends CI_Controller{
     
     public function __construct()
     {
-        //coba push
+        //coba push 2
         parent::__construct();
         $this->load->model('model_song');
     }
