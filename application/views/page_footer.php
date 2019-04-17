f1db1eec4b7f06620187ad73912af9ae18b017c8
vesheago <vesheafg@gmail.com>
2019-04-17 13:52:48 +0700

    footer

--- application/views/page_footer.php
+++ application/views/page_footer.php
@@ -1,1 +1,155 @@
-footer
+<div class="page_footer ">
+  <div class="footer">
+    <div>
+      <a href="/about">About Genius</a>
+      <a href="/contributor_guidelines">Contributor Guidelines</a>
+      <a href="/press" target="_blank">Press</a>
+      
+      
+        <a href="mailto:inquiry@genius.com">Advertise</a>
+      
+      <a href="https://eventspace.genius.com/">Event Space</a>
+    </div>
+
+    <div>
+      <a href="/static/privacy_policy" rel="nofollow" target="_blank">Privacy Policy</a>
+      
+      
+      <a href="/static/licensing" rel="nofollow" target="_blank">Licensing</a>
+      <a href="/jobs">Jobs</a>
+      <a href="/developers">Developers</a>
+      <a href="/static/terms" rel="nofollow" target="_blank">Terms of Use</a>
+      <a href="/static/copyright" rel="nofollow" target="_blank">Copyright Policy</a>
+      <a href="/feedback/new" ng-if="::false" rel="nofollow">Contact us</a>
+      <contact-button></contact-button>
+      
+  <a href="/login" class="facebox" rel="nofollow">Sign in</a>
+
+
+      
+    </div>
+
+    <div>
+      <span class="footer-copyright">&copy; 2019 Genius Media Group Inc.</span>
+    </div>
+  </div>
+
+  <div class="footer footer--secondary">
+  <a href="/verified-artists" class="footer-artist_links_label">Verified Artists</a>
+
+  <span class="footer-artist_links_label">All Artists:</span>
+  <ul class="characters_index_list">
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/a" class="character_index_list-link">A</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/b" class="character_index_list-link">B</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/c" class="character_index_list-link">C</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/d" class="character_index_list-link">D</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/e" class="character_index_list-link">E</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/f" class="character_index_list-link">F</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/g" class="character_index_list-link">G</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/h" class="character_index_list-link">H</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/i" class="character_index_list-link">I</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/j" class="character_index_list-link">J</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/k" class="character_index_list-link">K</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/l" class="character_index_list-link">L</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/m" class="character_index_list-link">M</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/n" class="character_index_list-link">N</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/o" class="character_index_list-link">O</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/p" class="character_index_list-link">P</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/q" class="character_index_list-link">Q</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/r" class="character_index_list-link">R</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/s" class="character_index_list-link">S</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/t" class="character_index_list-link">T</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/u" class="character_index_list-link">U</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/v" class="character_index_list-link">V</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/w" class="character_index_list-link">W</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/x" class="character_index_list-link">X</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/y" class="character_index_list-link">Y</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/z" class="character_index_list-link">Z</a>
+    </li>
+  
+    <li class="character_index_list-element">
+      <a href="https://genius.com/artists-index/0" class="character_index_list-link">#</a>
+    </li>
+  
+</ul>
+
+</div>
+
+</div>
