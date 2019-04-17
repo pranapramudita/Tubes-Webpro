5e11b094dc25259be2230436b00a77f5163c970b
pranapramudita <pranapramudita@gmail.com>
2019-04-17 11:37:53 +0700

    ga jadi ke azlyrics

--- application/views/page_header.php
+++ application/views/page_header.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
         <!-- title, icon, etc.-->
-        <title>AZLyrics - Song Lyrics from A to Z</title>
+        <title>Genius | Song Lyrics &amp; Knowledge</title>
         <link rel="shortcut icon" href="assets/az_logo_tr.png">
 
         <!-- Bootstrap CSS -->
@@ -16,7 +16,7 @@
 
     </head>
 
-    <body class="margin50">
+    <body>
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
@@ -24,61 +24,7 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
         
         <!-- code -->
-        <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#search-collapse"><span class="glyphicon glyphicon-search"></span></button>
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#artists-collapse"><span class="glyphicon glyphicon-th-list"></span></button>
-            <a class="navbar-brand" href="//www.azlyrics.com"><img alt="AZLyrics.com" class="pull-left" style="max-height:40px; margin-top:-10px;" src="//www.azlyrics.com/az_logo_tr.png"></a>
-            </div>
-            <ul class="collapse navbar-collapse nav navbar-nav" id="artists-collapse">
-            <li>
-            <div class="btn-group text-center" role="group">
-            <a class="btn btn-menu" href="//www.azlyrics.com/a.html">A</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/b.html">B</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/c.html">C</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/d.html">D</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/e.html">E</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/f.html">F</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/g.html">G</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/h.html">H</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/i.html">I</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/j.html">J</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/k.html">K</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/l.html">L</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/m.html">M</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/n.html">N</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/o.html">O</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/p.html">P</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/q.html">Q</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/r.html">R</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/s.html">S</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/t.html">T</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/u.html">U</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/v.html">V</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/w.html">W</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/x.html">X</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/y.html">Y</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/z.html">Z</a>
-            <a class="btn btn-menu" href="//www.azlyrics.com/19.html">#</a>
-            </div>
-            </li>
-            </ul>
-
-            <div class="collapse navbar-collapse" id="search-collapse">
-
-                <form class="navbar-form navbar-right search" method="get" action="//search.azlyrics.com/search.php" role="search">
-                <div class="input-group">  
-                <input type="text" class="form-control" placeholder="" name="q" id="q">
-                    <span class="input-group-btn">
-                        <button class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-search"></span> Search</button>
-                    </span>
-            </div>   
-            </form>
-
-            </div><!-- /.navbar-collapse -->
-            </div><!-- /.container -->
-        </nav>
+        
         
     </body>
 </html>
