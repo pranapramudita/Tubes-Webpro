2183fb2ef770cbaf20f3a7244464ed1a60539c36
pranapramudita <pranapramudita@gmail.com>
2019-04-19 10:33:01 +0700

    footer

---
+++ application/views/page_footer.php
@@ -0,0 +1,44 @@
+<!-- nav bottom -->
+<nav class="navbar navbar-default navbar-bottom">
+          <div class="container text-center">
+          <ul class="nav navbar-nav navbar-center">
+            <li><a href="#">Submit Lyrics</a></li>
+            <li><a href="#">Soundtracks</a></li>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Contact Us</a></li>
+         </ul>
+          </div> 
+        </nav>
+
+<!-- bot ban -->
+  <div class="lboard-wrap">
+  <div class="container">
+    <div class="row">
+       <div class="col-xs-12 top-ad text-center">
+          <span id="cf_banner_bottom"></span>
+       </div>
+    </div>
+  </div>
+  </div>
+
+<!-- footer -->
+<nav class="navbar navbar-footer">
+          <div class="container text-center">
+          <ul class="nav navbar-nav navbar-center">
+            <li><a href="#">Advertise Here</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">DMCA Policy</a></li>
+          </ul>
+          </div> 
+     </nav>
+     <div class="footer-wrap">
+          <div class="container">
+          <small>
+             All lyrics are property and copyright of their owners. All lyrics provided for educational purposes and personal use only.<br>
+             <script type="text/javascript">
+                curdate=new Date();
+                document.write("<strong>Copyright &copy; 2000-"+curdate.getFullYear()+" AZLyrics.com<\/strong>");
+             </script>
+          </small>
+          </div>
+     </div>
