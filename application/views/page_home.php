f3f6eb4ad23f51c93c379de4ebebdd5d4c4f1387
pranapramudita <pranapramudita@gmail.com>
2019-04-19 10:41:42 +0700

    assets album home

--- application/views/page_home.php
+++ application/views/page_home.php
@@ -1,59 +1,59 @@
 <!-- main -->
 <div class="container main-page">
-  <div class="row">
-             <div class="col-sm-6 col-lg-4 col-lg-offset-2 text-center artist-col">
-              <h1>Welcome to AZLyrics!</h1><br>
-              It's a place where all searches end!<br><br>
-              We have a large, legal, every day growing universe of lyrics where stars of all genres and ages shine.<br><br>
-              <form class="search" method="get" action="//search.azlyrics.com/search.php" role="search">
-               <div class="input-group">  
-		       <input type="text" class="form-control" placeholder="" name="q">
-       		   <span class="input-group-btn">
-            	  <button class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-search"></span></button>
-          	   </span>
- 	           </div>   
-	          </form>
-	          <p class="help-block">Enter artist name or song title</p>
-              </div>
-              <div class="col-sm-6 col-lg-4 text-center artist-col">
-               <div class="hidden-xs rect-ad">
-                 <span id="cf_medrec"></span>
-               </div>
-             </div>
-  </div>
-  <div class="row">
-             <div class="col-xs-12 col-lg-8 col-lg-offset-2 text-center artist-col">
-              <h1>WHAT'S HOT?</h1>
-             </div>
-  </div>
-<!-- what's hot placeholder -->
-<div class="row">
-  <div class="col-xs-6 col-sm-3 col-lg-2 col-lg-offset-2 albuma"><a href="/a/aaronlewis.html#65471"><img src="/hot/65471.jpg" alt="" /></a><a href="/a/aaronlewis.html#65471">Aaron Lewis</a> "State I'm In"</div>
-  <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/a/anderson-paak.html#66595"><img src="/hot/66595.jpg" alt="" /></a><a href="/a/anderson-paak.html#66595">Anderson .Paak</a> "Ventura"</div>
-  <div class="clearfix visible-xs-block"></div>
-  <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/b/bangtanboys.html#67033"><img src="/hot/67033.jpg" alt="" /></a><a href="/b/bangtanboys.html#67033">BTS</a> "MAP OF THE SOUL: PERSONA"</div>
-  <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/c/capitalbra.html#67484"><img src="/hot/67484.jpg" alt="" /></a><a href="/c/capitalbra.html#67484">Capital Bra</a> "CB6"</div>
-</div>
-<div class="row">
-  <div class="col-xs-6 col-sm-3 col-lg-2 col-lg-offset-2 albuma"><a href="/c/chemicalbrothers.html#65456"><img src="/hot/65456.jpg" alt="" /></a><a href="/c/chemicalbrothers.html#65456">The Chemical Brothers</a> "No Geography"</div>
-  <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/l/lsd.html#66591"><img src="/hot/66591.jpg" alt="" /></a><a href="/l/lsd.html#66591">LSD</a> "LABRINTH, SIA &amp; DIPLO PRESENT... LSD"</div>
-  <div class="clearfix visible-xs-block"></div>
-  <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/s/sebastianyatra.html#66862"><img src="/hot/66862.jpg" alt="" /></a><a href="/s/sebastianyatra.html#66862">Sebastian Yatra</a> "FANTASÍA"</div>
-  <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/w/walkingoncars.html#65702"><img src="/hot/65702.jpg" alt="" /></a><a href="/w/walkingoncars.html#65702">Walking On Cars</a> "Colours"</div>
-</div>
-<div class="row">
-  <div class="col-xs-6 col-sm-3 col-lg-2 col-lg-offset-2 albuma"><a href="/a/anitta.html#67220"><img src="/hot/67220.jpg" alt="" /></a><a href="/a/anitta.html#67220">Anitta</a> "Kisses"</div>
-  <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/b/blackpink.html#67139"><img src="/hot/67139.jpg" alt="" /></a><a href="/b/blackpink.html#67139">BLACKPINK</a> "Kill This Love"</div>
-  <div class="clearfix visible-xs-block"></div>
-  <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/d/drums.html#64927"><img src="/hot/64927.jpg" alt="" /></a><a href="/d/drums.html#64927">The Drums</a> "Brutalism"</div>
-  <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/k/khalid.html#66442"><img src="/hot/66442.jpg" alt="" /></a><a href="/k/khalid.html#66442">Khalid</a> "Free Spirit"</div>
-</div>
-<div class="row">
-  <div class="col-xs-6 col-sm-3 col-lg-2 col-lg-offset-2 albuma"><a href="/p/periphery.html#65537"><img src="/hot/65537.jpg" alt="" /></a><a href="/p/periphery.html#65537">Periphery</a> "Periphery IV: HAIL STAN"</div>
-  <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/p/pnl.html#67228"><img src="/hot/67228.jpg" alt="" /></a><a href="/p/pnl.html#67228">PNL</a> "Deux Frères"</div>
-  <div class="clearfix visible-xs-block"></div>
-  <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/r/rebamcentire.html#65813"><img src="/hot/65813.jpg" alt="" /></a><a href="/r/rebamcentire.html#65813">Reba McEntire</a> "Stronger Than The Truth"</div>
-  <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/s/sarabareilles.html#65915"><img src="/hot/65915.jpg" alt="" /></a><a href="/s/sarabareilles.html#65915">Sara Bareilles</a> "Amidst The Chaos"</div>
-</div>
-<!-- end of what's hot placeholder -->
-  </div>  <!-- container main-page -->
+    <div class="row">
+        <div class="col-sm-6 col-lg-4 col-lg-offset-2 text-center artist-col">
+            <h1>Welcome to AZLyrics!</h1><br>
+                It's a place where all searches end!<br><br>
+                We have a large, legal, every day growing universe of lyrics where stars of all genres and ages shine.<br><br>
+                <form class="search" method="get" action="//search.azlyrics.com/search.php" role="search">
+                    <div class="input-group">  
+                        <input type="text" class="form-control" placeholder="" name="q">
+                        <span class="input-group-btn">
+                            <button class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+                        </span>
+                    </div>
+	            </form>
+	            <p class="help-block">Enter artist name or song title</p>
+        </div>
+        <div class="col-sm-6 col-lg-4 text-center artist-col">
+            <div class="hidden-xs rect-ad">
+                <span id="cf_medrec"></span>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-lg-8 col-lg-offset-2 text-center artist-col">
+            <h1>WHAT'S HOT?</h1>
+        </div>
+    </div>
+    <!-- what's hot placeholder -->
+    <div class="row">
+        <div class="col-xs-6 col-sm-3 col-lg-2 col-lg-offset-2 albuma"><a href="/a/aaronlewis.html#65471"><img src="assets/65471.jpg" alt="" /></a><a href="/a/aaronlewis.html#65471">Aaron Lewis</a> "State I'm In"</div>
+        <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/a/anderson-paak.html#66595"><img src="assets/66595.jpg" alt="" /></a><a href="/a/anderson-paak.html#66595">Anderson .Paak</a> "Ventura"</div>
+        <div class="clearfix visible-xs-block"></div>
+        <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/b/bangtanboys.html#67033"><img src="assets/67033.jpg" alt="" /></a><a href="/b/bangtanboys.html#67033">BTS</a> "MAP OF THE SOUL: PERSONA"</div>
+        <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/c/capitalbra.html#67484"><img src="assets/67484.jpg" alt="" /></a><a href="/c/capitalbra.html#67484">Capital Bra</a> "CB6"</div>
+    </div>
+    <div class="row">
+        <div class="col-xs-6 col-sm-3 col-lg-2 col-lg-offset-2 albuma"><a href="/c/chemicalbrothers.html#65456"><img src="assets/65456.jpg" alt="" /></a><a href="/c/chemicalbrothers.html#65456">The Chemical Brothers</a> "No Geography"</div>
+        <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/l/lsd.html#66591"><img src="assets/66591.jpg" alt="" /></a><a href="/l/lsd.html#66591">LSD</a> "LABRINTH, SIA &amp; DIPLO PRESENT... LSD"</div>
+        <div class="clearfix visible-xs-block"></div>
+        <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/s/sebastianyatra.html#66862"><img src="assets/66862.jpg" alt="" /></a><a href="/s/sebastianyatra.html#66862">Sebastian Yatra</a> "FANTASÍA"</div>
+        <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/w/walkingoncars.html#65702"><img src="assets/65702.jpg" alt="" /></a><a href="/w/walkingoncars.html#65702">Walking On Cars</a> "Colours"</div>
+    </div>
+    <div class="row">
+        <div class="col-xs-6 col-sm-3 col-lg-2 col-lg-offset-2 albuma"><a href="/a/anitta.html#67220"><img src="assets/67220.jpg" alt="" /></a><a href="/a/anitta.html#67220">Anitta</a> "Kisses"</div>
+        <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/b/blackpink.html#67139"><img src="assets/67139.jpg" alt="" /></a><a href="/b/blackpink.html#67139">BLACKPINK</a> "Kill This Love"</div>
+        <div class="clearfix visible-xs-block"></div>
+        <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/d/drums.html#64927"><img src="assets/64927.jpg" alt="" /></a><a href="/d/drums.html#64927">The Drums</a> "Brutalism"</div>
+        <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/k/khalid.html#66442"><img src="assets/66442.jpg" alt="" /></a><a href="/k/khalid.html#66442">Khalid</a> "Free Spirit"</div>
+    </div>
+    <div class="row">
+        <div class="col-xs-6 col-sm-3 col-lg-2 col-lg-offset-2 albuma"><a href="/p/periphery.html#65537"><img src="assets/65537.jpg" alt="" /></a><a href="/p/periphery.html#65537">Periphery</a> "Periphery IV: HAIL STAN"</div>
+        <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/p/pnl.html#67228"><img src="assets/67228.jpg" alt="" /></a><a href="/p/pnl.html#67228">PNL</a> "Deux Frères"</div>
+        <div class="clearfix visible-xs-block"></div>
+        <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/r/rebamcentire.html#65813"><img src="assets/65813.jpg" alt="" /></a><a href="/r/rebamcentire.html#65813">Reba McEntire</a> "Stronger Than The Truth"</div>
+        <div class="col-xs-6 col-sm-3 col-lg-2 albuma"><a href="/s/sarabareilles.html#65915"><img src="assets/65915.jpg" alt="" /></a><a href="/s/sarabareilles.html#65915">Sara Bareilles</a> "Amidst The Chaos"</div>
+    </div>
+    <!-- end of what's hot placeholder -->
+</div>  <!-- container main-page -->
