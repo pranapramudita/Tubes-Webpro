7a03365facc83d5b8333a87973f7e2981aa00060
pranapramudita <pranapramudita@gmail.com>
2019-04-21 06:57:32 +0700

    database taruh di folder

--- Database/song.sql
+++ Database/song.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 19 Apr 2019 pada 00.57
+-- Generation Time: 20 Apr 2019 pada 11.31
 -- Versi Server: 10.1.13-MariaDB
 -- PHP Version: 7.0.8
 
@@ -17,52 +17,33 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tubes_webpro`
+-- Database: `tubes-webpro`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `account`
+-- Struktur dari tabel `album`
 --
 
-CREATE TABLE `account` (
-  `id_user` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `bio` varchar(255) NOT NULL,
-  `profil` longblob NOT NULL
+CREATE TABLE `album` (
+  `id_album` varchar(255) NOT NULL,
+  `id_artist` varchar(255) NOT NULL,
+  `id_song` varchar(255) NOT NULL,
+  `album_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `articles`
+-- Struktur dari tabel `artist`
 --
 
-CREATE TABLE `articles` (
-  `id_art` varchar(255) NOT NULL,
-  `art_title` varchar(255) NOT NULL,
-  `date` date NOT NULL,
-  `content` longtext NOT NULL,
-  `image` longblob NOT NULL,
-  `video` longblob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `shop`
---
-
-CREATE TABLE `shop` (
-  `id_shop` varchar(255) NOT NULL,
-  `size` varchar(255) NOT NULL,
-  `price` int(11) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `image` longblob NOT NULL
+CREATE TABLE `artist` (
+  `id_artist` varchar(255) NOT NULL,
+  `id_song` varchar(255) NOT NULL,
+  `id_album` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -73,12 +54,11 @@ CREATE TABLE `shop` (
 
 CREATE TABLE `song` (
   `id_song` varchar(255) NOT NULL,
-  `lyrics` longtext NOT NULL,
+  `id_artist` varchar(255) NOT NULL,
+  `id_album` varchar(255) NOT NULL,
   `song_title` varchar(255) NOT NULL,
-  `produce` varchar(255) NOT NULL,
-  `album` varchar(255) NOT NULL,
-  `singer` varchar(255) NOT NULL,
-  `image` longblob NOT NULL
+  `producer` varchar(255) NOT NULL,
+  `lyrics` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -86,28 +66,30 @@ CREATE TABLE `song` (
 --
 
 --
--- Indexes for table `account`
+-- Indexes for table `album`
 --
-ALTER TABLE `account`
-  ADD PRIMARY KEY (`id_user`);
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`id_album`),
+  ADD KEY `id_artist` (`id_artist`),
+  ADD KEY `id_song` (`id_song`);
 
 --
--- Indexes for table `articles`
+-- Indexes for table `artist`
 --
-ALTER TABLE `articles`
-  ADD PRIMARY KEY (`id_art`);
-
---
--- Indexes for table `shop`
---
-ALTER TABLE `shop`
-  ADD PRIMARY KEY (`id_shop`);
+ALTER TABLE `artist`
+  ADD PRIMARY KEY (`id_artist`),
+  ADD UNIQUE KEY `id_song` (`id_song`),
+  ADD KEY `id_song_2` (`id_song`),
+  ADD KEY `id_song_3` (`id_song`),
+  ADD KEY `id_album` (`id_album`);
 
 --
 -- Indexes for table `song`
 --
 ALTER TABLE `song`
-  ADD PRIMARY KEY (`id_song`);
+  ADD PRIMARY KEY (`id_song`),
+  ADD KEY `id_artist` (`id_artist`),
+  ADD KEY `id_album` (`id_album`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
