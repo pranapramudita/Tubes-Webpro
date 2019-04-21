d28bc9ea1500fdc438fd7ff48603a4942352a3e4
pranapramudita <pranapramudita@gmail.com>
2019-04-21 08:06:51 +0700

    page song added

--- application/views/page_song.php
+++ application/views/page_song.php
@@ -1,2 +1,52 @@
-song
-test
+<!-- main -->
+<div class="container main-page">
+    <div class="row">
+        <div class="col-md-3 text-center hidden-sm hidden-xs">
+            <div class="sky-ad"></div>
+        </div>
+
+        <!-- content -->
+        <div class="col-xs-12 col-md-6 text-center">
+
+            <form id="addsong" action="../add.php" method="post">
+            <input type="hidden" name="what" value="add_song">
+            <input type="hidden" name="artist" value="Aaron Lewis">
+            <input type="hidden" name="artist_id" value="2713">
+            </form>
+
+            <!-- <div class="div-share noprint">
+            <div class="fb-like" style="float:left;" data-href="//www.azlyrics.com/a/aaronlewis.html" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+            </div> -->
+
+            <h1><strong>Aaron Lewis Lyrics</strong></h1>
+
+            <!-- <div class="ringtone">
+            <span id="cf_text_top"></span>
+            </div> -->
+
+            Related artists:<br>
+            <!-- <a href="../s/staind.html">Staind</a>
+            <br><br> -->
+
+            <!-- start of song list -->
+            <a href="#" class="btn btn-xs btn-default sorting" onclick="return showAlbum();"><span class="glyphicon glyphicon-sort-by-order"></span> sort by album</a><a href="#" class="btn btn-xs btn-default sorting" onclick="return showSong();"><span class="glyphicon glyphicon-sort-by-alphabet"></span> sort by song</a><br>
+
+            <!-- Perulangan list album -->
+            <div id="23325" class="album">album: <b>"The Road"</b> (2012)</div>
+            <!-- Perulangan list lagu -->
+            <a href="../lyrics/aaronlewis/75.html" target="_blank">75</a><br>
+            <!-- end -->
+            </div>
+            <!-- end of song list -->
+
+            <!-- <div class="ringtone">
+            <span id="cf_text_bottom"></span>
+            </div> -->
+
+        </div> <!-- content -->
+
+    <!-- <div class="col-md-3 text-center hidden-sm hidden-xs">
+    <div class="sky-ad"></div>
+    </div> -->
+    </div>
+</div>  <!-- container main-page -->
