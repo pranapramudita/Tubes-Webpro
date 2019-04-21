9e84fe65ad2de53a28ca048d471fe970685358e6
pranapramudita <pranapramudita@gmail.com>
2019-04-21 07:21:40 +0700

    rapihin page_submit

--- application/views/page_search.php
+++ application/views/page_search.php
@@ -1,1 +1,52 @@
-search
+search<div class="container main-page">
+    <div class="row">
+        <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 text-center" style="padding-top: 20px;">
+            
+            <div class="panel">
+                <div class="panel-heading"><b>Artist results:</b><br><small>[1-1 of 1 total <span class="text-lowercase">Artists</span> found]</small></div>
+                <table class="table table-condensed">
+                    
+                    
+            <tr>
+                <td class="text-left visitedlyr">
+                    1. <a href="https://www.azlyrics.com/j/joji.html" target="_blank"><b>Joji</b></a>
+                </td>
+            </tr>
+            
+                    
+                </table>
+            </div>
+            
+            <div class="panel">
+                <div class="panel-heading"><b>Song results:</b><br><small>[1-5 of 56 total <span class="text-lowercase">Songs</span> found]</small></div>
+                <table class="table table-condensed">
+                    
+                    
+            <tr>
+                <td class="text-left visitedlyr">
+                    1. <a href="https://www.azlyrics.com/lyrics/joji/rip.html" target="_blank"><b>R.I.P.</b></a>  by <b>Joji</b><br>
+                    <small>[<strong>Joji</strong>:] There's no more time Just lay it on me If I lost my life You can blame it on me Are you ready or not? We can start right here Take you on them rocks So crystal clear Oh, oh, oh Are they talkin' 'bout us? Are they talkin' 'bout us? (Yeah) Yeah,...</small>
+                </td>
+            </tr>
+                
+            <tr>
+                <td>
+                    <a class="btn btn-share" href="more">More Song Results</a>
+                </td>
+            </tr>
+            
+                    
+                </table>
+            </div>
+            
+            <form class="search" method="get" action="" role="search">
+                <div style="margin-bottom:15px" class="input-group">
+                    <input type="text" class="form-control" placeholder="" name="q" value="joji">
+                    <span class="input-group-btn">
+            	  <button class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-search"></span> Search</button>
+          	</span>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
