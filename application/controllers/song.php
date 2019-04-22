624382b8d9be8bbe13d1fb187d2355b18df0fe49
vesheago <vesheafg@gmail.com>
2019-04-22 21:55:14 +0700

    coba upload gambar

--- application/controllers/song.php
+++ application/controllers/song.php
@@ -81,11 +81,30 @@ class song extends CI_Controller{
             "artist" => $this->input->post('artist', true),
             "album" => $this->input->post('album', true),
             "lyrics" => $this->input->post('lyrics', true),
+            "image" => $this->input->post('userfile', true),
         ];
         $this->model_song->insert_lyrics($data);
+        $this->upload();
         redirect('submit','refresh');
     }
-    
+
+    #upload function
+    public function upload(){
+        $config['upload_path'] = './assets/';
+        $config['allowed_types'] = 'gif|jpg|png';
+        $config['max_size']  = '4096';
+        $config['max_width']  = '1920';
+        $config['max_height']  = '1080';
+        
+        $this->load->library('upload', $config);
+        
+        if (!$this->upload->do_upload()){
+            $error = array('error' => $this->upload->display_errors());
+        }
+        else{
+            $data = array('upload_data' => $this->upload->data());
+        }
+    }
 
     #contact function
     public function contact(){
