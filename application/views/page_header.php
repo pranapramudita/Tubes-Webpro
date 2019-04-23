7d3f5cfcf11c47e506470651f4ede011d3d9ad0e
pranapramudita <pranapramudita@gmail.com>
2019-04-23 22:36:47 +0700

    FINAL FIX FINISH kayanya

--- application/views/page_header.php
+++ application/views/page_header.php
@@ -66,7 +66,7 @@
 
             <div class="collapse navbar-collapse" id="search-collapse">
 
-            <form class="navbar-form navbar-right search" method="post" action="<?php echo base_url() ?>index.php/song/search">
+            <form class="navbar-form navbar-right search" method="post" action="<?php echo base_url() ?>index.php/song/search/">
                 <div class="input-group">  
                 <input type="text" class="form-control" placeholder="" name="keyword" id="q">
                     <span class="input-group-btn">
